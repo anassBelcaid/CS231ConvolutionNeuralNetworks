a20206f92c9f6e178d0f920aaa0df4ab8fa399b1
anass <anass.belcaid@gmail.com>
2018-10-30 16:08:31 +0000

    Solution affine layer

--- assignments/assignment2/cs231n/layers.py
+++ assignments/assignment2/cs231n/layers.py
@@ -25,7 +25,12 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
-    pass
+    #reshaping the input in size( N*D)
+    N = x.shape[0]
+    X = np.reshape(x,(N,-1))
+
+    #computing the output
+    out = np.dot(X,w)+b
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -54,7 +59,21 @@ def affine_backward(dout, cache):
     ###########################################################################
     # TODO: Implement the affine backward pass.                               #
     ###########################################################################
-    pass
+    #dx Gradient
+    dx = np.dot(dout,w.T)
+    dx = np.reshape(dx,x.shape)
+    
+
+    #dw Gradient
+    N= dout.shape[0]
+
+    #reshape x 
+    x_resh = np.reshape(x,(N,-1))
+    dw = np.dot(x_resh.T,dout)
+
+    #computing db gradient
+    db = np.dot(dout.T,np.ones(N))
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
