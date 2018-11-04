Layer Normalisation, batch normalisation bug fix

assignments/assignment2/cs231n/classifiers/fc_net.py
@@ -200,13 +200,19 @@ class FullyConnectedNet(object):
         self.params['W1'] = np.random.normal(loc = 0, size= (input_dim,
             hidden_dims[0]), scale = weight_scale)
         self.params['b1'] = np.zeros(hidden_dims[0])
+        if self.normalization=='batchnorm':
+            self.params['gamma1']= np.random.normal(loc=0,size=hidden_dims[0])
+            self.params['beta1'] = np.random.normal(loc=0,size=hidden_dims[0])
+        
 
         #initializing the rest of the keys
         k=2
         for (d1,d2)  in zip(hidden_dims[:-1],hidden_dims[1:]):
-            self.params['W%d'%k] = np.random.normal(loc = 0, size = (d1,d2),
-                    scale = weight_scale)
+            self.params['W%d'%k] = np.random.normal(loc = 0, size = (d1,d2), scale = weight_scale)
             self.params['b%d'%k] = np.zeros(d2)
+            if self.normalization=='batchnorm':
+                self.params['gamma%d'%k]=np.random.normal(loc=0,size=d2)
+                self.params['beta%d'%k] = np.random.normal(loc=0,size=d2)
 
             k+=1
 
@@ -286,8 +292,15 @@ class FullyConnectedNet(object):
         for i in range(self.num_layers-1):
             #extracting the coefficients
             w, b = self.params['W%d'%k], self.params['b%d'%k]
-            #computing the combined relu layer
-            layer,self.caches[k] = affine_relu_forward(layer, w, b)
+
+            #check normaisation
+            if self.normalization=='batchnorm':
+                beta, gamma = self.params['beta%d'%k], self.params['gamma%d'%k]
+                layer, self.caches[k] = affine_relu_norm_forward(layer, w, b,
+                        gamma, beta, self.bn_params[i])
+            else:
+                #computing the combined relu layer
+                layer,self.caches[k] = affine_relu_forward(layer, w, b)
 
             #next level
             k+=1
@@ -295,12 +308,6 @@ class FullyConnectedNet(object):
         #softmax layer
         w,b = self.params['W%d'%k], self.params['b%d'%k]
         scores,self.caches[k] = affine_forward(layer, w,b)
-
-
-
-
-
-
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -335,7 +342,11 @@ class FullyConnectedNet(object):
         k-=1
         #loop over the rest of the layer
         while(k>0):
-            dout, grads['W%d'%k], grads['b%d'%k] = affine_relu_backward(dout,self.caches[k])
+            if self.normalization=='batchnorm':
+                dout,grads['W%d'%k],grads['b%d'%k],grads['gamma%d'%k],grads['beta%d'%k] = affine_relu_norm_backward(dout,self.caches[k])
+            else:
+                dout, grads['W%d'%k], grads['b%d'%k] = affine_relu_backward(dout,self.caches[k])
+
             k-=1
 
 

assignments/assignment2/cs231n/layers.py
@@ -372,7 +372,18 @@ def layernorm_forward(x, gamma, beta, ln_param):
     # transformations you could perform, that would enable you to copy over   #
     # the batch norm code and leave it almost unchanged?                      #
     ###########################################################################
-    pass
+    mean = np.mean(x, axis=1, keepdims=True)
+    var =  np.var(x, axis=1, keepdims=True)
+
+
+    #computing the xhat transformation
+    out = (x -mean)/np.sqrt(var+eps)
+
+    #linear transformation 
+    out = gamma*out + beta
+
+    #saving the cache
+    cache = x, mean, var, gamma, beta,eps
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -403,7 +414,32 @@ def layernorm_backward(dout, cache):
     # implementation of batch normalization. The hints to the forward pass    #
     # still apply!                                                            #
     ###########################################################################
-    pass
+    x, mean , var, gamma, beta,eps = cache
+    m = dout.shape[1]
+    x_hat= (x-mean)/np.sqrt(var+eps)
+    dx = np.zeros_like(x)
+
+    #Computing the easiest one (dbeta :) ) 
+    dbeta = np.sum(dout,axis=0)
+
+    #now for dgamma
+    dgamma = np.sum(dout*x_hat, axis=0) 
+
+
+    #now for the most difficult one
+    dx_hat = dout * gamma
+    
+    #gradient over the std
+    d_sigma = dx_hat* (x-mean)* (-0.5*( var+eps)**(-3/2))
+    d_sigma= np.sum(d_sigma, axis=1,keepdims=True)
+
+    # gradient over the mean
+    d_mean = dx_hat *( -1/np.sqrt(var+eps))
+    d_mean = np.sum(d_mean,axis=1,keepdims=True)
+    d_mean += d_sigma* (np.mean(-2*(x-mean),axis=1,keepdims=True))
+    
+    #Moment of truth
+    dx = dx_hat * 1/np.sqrt(var+eps) + (2/m)*d_sigma*(x-mean) +(1/m)* d_mean
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################